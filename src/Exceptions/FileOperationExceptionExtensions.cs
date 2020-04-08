49f41453eab080ec37ed04ddb5b3192c2791418e
Vladyslav Zaiets <rutova2@gmail.com>
2020-04-08 20:39:14 +0300

    feat(exceptions): extension methods on FileOperationException

--- src/Exceptions/FileOperationExceptionExtensions.cs
+++ src/Exceptions/FileOperationExceptionExtensions.cs
@@ -4,42 +4,69 @@ using System.IO;
 namespace FFmpegDotnetWrapper.Exceptions
 {
     /// <summary>
-    /// Extension methods for <see cref="FileOperationException"/>.
+    /// Provides extension methods for <see cref="FileOperationException"/> to facilitate common file operation error handling scenarios.
     /// </summary>
     public static class FileOperationExceptionExtensions
     {
         /// <summary>
-        /// Returns the file name component of the <see cref="FileOperationException.FilePath"/>.
-        /// If <see cref="FileOperationException.FilePath"/> is null or empty, returns an empty string.
+        /// Gets the file name component from the <see cref="FileOperationException.FilePath"/> property.
         /// </summary>
+        /// <param name="ex">The exception instance.</param>
+        /// <returns>The file name extracted from the path, or an empty string if the path is null, empty, or whitespace.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
         public static string GetFileName(this FileOperationException ex)
         {
-            if (string.IsNullOrWhiteSpace(ex.FilePath))
-                return string.Empty;
+            ArgumentNullException.ThrowIfNull(ex);
 
-            return Path.GetFileName(ex.FilePath);
+            return string.IsNullOrWhiteSpace(ex.FilePath)
+                ? string.Empty
+                : Path.GetFileName(ex.FilePath);
         }
 
         /// <summary>
-        /// Indicates whether the exception contains a non‑empty file path.
+        /// Determines whether the exception contains a non-empty file path.
         /// </summary>
-        public static bool HasFilePath(this FileOperationException ex) =>
-            !string.IsNullOrWhiteSpace(ex.FilePath);
+        /// <param name="ex">The exception instance.</param>
+        /// <returns><see langword="true"/> if the exception has a non-empty file path; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
+        public static bool HasFilePath(this FileOperationException ex)
+        {
+            ArgumentNullException.ThrowIfNull(ex);
+            return !string.IsNullOrWhiteSpace(ex.FilePath);
+        }
 
         /// <summary>
-        /// Formats the exception as a single loggable string, including the message and file path (if any).
+        /// Formats the exception as a single loggable string, including the message and file path (if present).
         /// </summary>
-        public static string ToLogString(this FileOperationException ex) =>
-            $"Error: {ex.Message}{(ex.HasFilePath() ? $" (File: {ex.FilePath})" : string.Empty)}";
+        /// <param name="ex">The exception instance.</param>
+        /// <returns>A formatted string suitable for logging purposes.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
+        public static string ToLogString(this FileOperationException ex)
+        {
+            ArgumentNullException.ThrowIfNull(ex);
+
+            return ex.HasFilePath()
+                ? $"Error: {ex.Message} (File: {ex.FilePath})"
+                : $"Error: {ex.Message}";
+        }
 
         /// <summary>
-        /// Creates a new <see cref="FileOperationException"/> that appends additional context to the original message.
-        /// The original exception is set as the inner exception.
+        /// Creates a new <see cref="FileOperationException"/> with additional context appended to the original message.
+        /// The original exception is preserved as the inner exception.
         /// </summary>
+        /// <param name="ex">The original exception instance.</param>
+        /// <param name="additionalInfo">Additional context information to append to the error message.</param>
+        /// <returns>A new <see cref="FileOperationException"/> with combined message and preserved file path.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="ex"/> is <see langword="null"/>.
+        /// <paramref name="additionalInfo"/> is <see langword="null"/>.
+        /// </exception>
         public static FileOperationException WithAdditionalInfo(this FileOperationException ex, string additionalInfo)
         {
+            ArgumentNullException.ThrowIfNull(ex);
+            ArgumentNullException.ThrowIfNull(additionalInfo);
+
             var combinedMessage = $"{ex.Message} - {additionalInfo}";
-            // Preserve the original file path if present; otherwise pass an empty string.
             var filePath = ex.FilePath ?? string.Empty;
             return new FileOperationException(combinedMessage, filePath, ex);
         }
