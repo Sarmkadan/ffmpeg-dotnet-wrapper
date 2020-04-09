528b71e4f61daf1a5260b428fc95052ac59a9c83
Vladyslav Zaiets <rutova2@gmail.com>
2020-04-09 08:27:06 +0300

    ValidationException extensinos.

--- src/Exceptions/ValidationExceptionExtensions.cs
+++ src/Exceptions/ValidationExceptionExtensions.cs
@@ -13,36 +13,54 @@ namespace FFmpegDotnetWrapper.Exceptions
         /// <summary>
         /// Flattens all validation error messages into a single enumerable.
         /// </summary>
+        /// <param name="ex">The validation exception instance.</param>
+        /// <returns>An enumerable containing all validation error messages across all fields.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
         public static IEnumerable<string> GetAllErrorMessages(this ValidationException ex)
         {
-            if (ex.ValidationErrors == null)
-                return Enumerable.Empty<string>();
+            ArgumentNullException.ThrowIfNull(ex);
 
-            return ex.ValidationErrors.Values.SelectMany(v => v);
+            return ex.ValidationErrors?.Values.SelectMany(v => v) ?? Enumerable.Empty<string>();
         }
 
         /// <summary>
         /// Determines whether the exception contains any errors for the specified field.
         /// </summary>
+        /// <param name="ex">The validation exception instance.</param>
+        /// <param name="field">The field name to check for errors.</param>
+        /// <returns><see langword="true"/> if the exception has errors for the specified field; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="field"/> is <see langword="null"/> or empty.</exception>
         public static bool HasErrorForField(this ValidationException ex, string field)
         {
-            if (ex.ValidationErrors == null || string.IsNullOrEmpty(field))
-                return false;
+            ArgumentNullException.ThrowIfNull(ex);
+            ArgumentException.ThrowIfNullOrEmpty(field, nameof(field));
 
-            return ex.ValidationErrors.TryGetValue(field, out var errors) && errors.Length > 0;
+            return ex.ValidationErrors?.TryGetValue(field, out var errors) == true && errors.Length > 0;
         }
 
         /// <summary>
         /// Returns a new <see cref="ValidationException"/> that includes an additional error for the given field.
         /// The original exception is left unchanged.
         /// </summary>
+        /// <param name="ex">The original validation exception instance.</param>
+        /// <param name="field">The field name to add the error to.</param>
+        /// <param name="errorMessage">The error message to add.</param>
+        /// <returns>A new <see cref="ValidationException"/> with the additional error included.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="ex"/> is <see langword="null"/>.
+        /// <paramref name="field"/> is <see langword="null"/>.
+        /// <paramref name="errorMessage"/> is <see langword="null"/>.
+        /// </exception>
         public static ValidationException WithAddedError(this ValidationException ex, string field, string errorMessage)
         {
-            if (string.IsNullOrEmpty(field))
-                throw new ArgumentException("Field name cannot be null or empty.", nameof(field));
+            ArgumentNullException.ThrowIfNull(ex);
+            ArgumentException.ThrowIfNullOrEmpty(field, nameof(field));
+            ArgumentNullException.ThrowIfNull(errorMessage);
 
-            // Copy existing errors (if any) into a new dictionary.
-            var newDict = new Dictionary<string, string[]>(ex.ValidationErrors ?? new Dictionary<string, string[]>(), StringComparer.OrdinalIgnoreCase);
+            // Copy existing errors (if any) into a new dictionary
+            var existingErrors = ex.ValidationErrors ?? new Dictionary<string, string[]>(StringComparer.OrdinalIgnoreCase);
+            var newDict = new Dictionary<string, string[]>(existingErrors, StringComparer.OrdinalIgnoreCase);
 
             if (newDict.TryGetValue(field, out var existing))
             {
@@ -60,12 +78,17 @@ namespace FFmpegDotnetWrapper.Exceptions
         /// <summary>
         /// Produces a detailed string representation of the exception, including all validation errors.
         /// </summary>
+        /// <param name="ex">The validation exception instance.</param>
+        /// <returns>A formatted string containing the exception message and all validation errors.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
         public static string ToDetailedString(this ValidationException ex)
         {
+            ArgumentNullException.ThrowIfNull(ex);
+
             var sb = new StringBuilder();
             sb.AppendLine(ex.Message);
 
-            if (ex.ValidationErrors != null && ex.ValidationErrors.Count > 0)
+            if (ex.ValidationErrors is { Count: > 0 })
             {
                 foreach (var kvp in ex.ValidationErrors)
                 {
