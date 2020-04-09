d937f362977125c4fb988d480f1125c53ff24432
Vladyslav Zaiets <rutova2@gmail.com>
2020-04-09 16:05:32 +0300

    FFmpegExceptionJson (de)serialization helpers

--- src/Exceptions/FFmpegExceptionJsonExtensions.cs
+++ src/Exceptions/FFmpegExceptionJsonExtensions.cs
@@ -1,7 +1,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using System.Text.Json;
 
@@ -12,74 +12,72 @@ namespace FFmpegDotnetWrapper.Exceptions;
 /// </summary>
 public static class FFmpegExceptionJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new()
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false
+	};
 
-    /// <summary>
-    /// Serializes the FFmpegException to a JSON string.
-    /// </summary>
-    /// <param name="value">The exception to serialize.</param>
-    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the exception.</returns>
-    public static string ToJson(this FFmpegException value, bool indented = false)
-    {
-        if (value == null)
-        {
-            throw new ArgumentNullException(nameof(value));
-        }
+	/// <summary>
+	/// Serializes the FFmpegException to a JSON string.
+	/// </summary>
+	/// <param name="value">The exception to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the exception.</returns>
+	/// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+	public static string ToJson(this FFmpegException value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true
-            }
-            : _jsonOptions;
+		var options = indented
+			? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+			: _jsonOptions;
 
-        return JsonSerializer.Serialize(value, options);
-    }
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    /// <summary>
-    /// Deserializes a JSON string to an FFmpegException instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized FFmpegException, or null if the JSON is null or whitespace.</returns>
-    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
-    public static FFmpegException? FromJson(string json)
-    {
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return null;
-        }
+	/// <summary>
+	/// Deserializes a JSON string to an FFmpegException instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized FFmpegException, or <see langword="null"/> if the JSON is <see langword="null"/>, empty, or whitespace.</returns>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+	/// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+	public static FFmpegException? FromJson(string json)
+	{
+		ArgumentException.ThrowIfNullOrEmpty(json);
 
-        return JsonSerializer.Deserialize<FFmpegException>(json, _jsonOptions);
-    }
+		return string.IsNullOrWhiteSpace(json)
+			? null
+			: JsonSerializer.Deserialize<FFmpegException>(json, _jsonOptions);
+	}
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string to an FFmpegException instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized exception if successful.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    public static bool TryFromJson(string json, out FFmpegException? value)
-    {
-        value = null;
+	/// <summary>
+	/// Attempts to deserialize a JSON string to an FFmpegException instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized exception if successful.</param>
+	/// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+	/// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+	public static bool TryFromJson(string json, out FFmpegException? value)
+	{
+		ArgumentException.ThrowIfNullOrEmpty(json);
 
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return false;
-        }
+		value = null;
 
-        try
-        {
-            value = JsonSerializer.Deserialize<FFmpegException>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            return false;
-        }
-    }
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return false;
+		}
+
+		try
+		{
+			value = JsonSerializer.Deserialize<FFmpegException>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			return false;
+		}
+	}
 }
