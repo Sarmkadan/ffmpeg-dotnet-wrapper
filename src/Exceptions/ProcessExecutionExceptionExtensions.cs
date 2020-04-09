b2fa43db450d4d846c6ca6d1abdde136462575ff
Vladyslav Zaiets <rutova2@gmail.com>
2020-04-09 19:58:29 +0300

    Small ProcessExecutionException helpers

--- src/Exceptions/ProcessExecutionExceptionExtensions.cs
+++ src/Exceptions/ProcessExecutionExceptionExtensions.cs
@@ -2,30 +2,61 @@ using System;
 
 namespace FFmpegDotnetWrapper.Exceptions
 {
-    public static class ProcessExecutionExceptionExtensions
-    {
-        public static bool IsSuccessful(this ProcessExecutionException ex)
-        {
-            return ex.ExitCode == 0;
-        }
+	/// <summary>
+	/// Provides extension methods for <see cref="ProcessExecutionException"/> to facilitate error handling and diagnostics.
+	/// </summary>
+	public static class ProcessExecutionExceptionExtensions
+	{
+		/// <summary>
+		/// Determines whether the process execution was successful based on the exit code.
+		/// </summary>
+		/// <param name="ex">The exception containing process execution information.</param>
+		/// <returns><see langword="true"/> if the exit code is 0; otherwise, <see langword="false"/>.</returns>
+		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/></exception>
+		public static bool IsSuccessful(this ProcessExecutionException ex)
+		{
+			ArgumentNullException.ThrowIfNull(ex);
+			return ex.ExitCode == 0;
+		}
 
-        public static string GetErrorMessage(this ProcessExecutionException ex)
-        {
-            return ex.ErrorOutput ?? ex.Message;
-        }
+		/// <summary>
+		/// Gets the primary error message from the exception, preferring <see cref="ProcessExecutionException.ErrorOutput"/> over <see cref="Exception.Message"/>.
+		/// </summary>
+		/// <param name="ex">The exception containing process execution information.</param>
+		/// <returns>The error output if available; otherwise, the exception message.</returns>
+		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/></exception>
+		public static string GetErrorMessage(this ProcessExecutionException ex)
+		{
+			ArgumentNullException.ThrowIfNull(ex);
+			return ex.ErrorOutput ?? ex.Message;
+		}
 
-        public static string GetDetailedErrorMessage(this ProcessExecutionException ex)
-        {
-            var errorMessage = ex.GetErrorMessage();
-            var exitCode = ex.ExitCode.HasValue ? $"Exit code: {ex.ExitCode}" : "No exit code available";
-            return $"{errorMessage} ({exitCode})";
-        }
+		/// <summary>
+		/// Gets a detailed error message that includes both the error output and the process exit code.
+		/// </summary>
+		/// <param name="ex">The exception containing process execution information.</param>
+		/// <returns>A formatted string containing the error message and exit code information.</returns>
+		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/></exception>
+		public static string GetDetailedErrorMessage(this ProcessExecutionException ex)
+		{
+			ArgumentNullException.ThrowIfNull(ex);
+			var errorMessage = ex.GetErrorMessage();
+			var exitCode = ex.ExitCode.HasValue ? $"Exit code: {ex.ExitCode}" : "No exit code available";
+			return $"{errorMessage} ({exitCode})\n";
+		}
 
-        public static string GetFullExceptionDetails(this ProcessExecutionException ex)
-        {
-            var detailedErrorMessage = ex.GetDetailedErrorMessage();
-            var innerExceptionMessage = ex.InnerException != null ? $"Inner exception: {ex.InnerException.Message}" : "No inner exception";
-            return $"{detailedErrorMessage}\n{innerExceptionMessage}";
-        }
-    }
+		/// <summary>
+		/// Gets comprehensive exception details including the detailed error message and inner exception information.
+		/// </summary>
+		/// <param name="ex">The exception containing process execution information.</param>
+		/// <returns>A formatted string containing the detailed error message and inner exception details.</returns>
+		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/></exception>
+		public static string GetFullExceptionDetails(this ProcessExecutionException ex)
+		{
+			ArgumentNullException.ThrowIfNull(ex);
+			var detailedErrorMessage = ex.GetDetailedErrorMessage();
+			var innerExceptionMessage = ex.InnerException != null ? $"Inner exception: {ex.InnerException.Message}" : "No inner exception";
+			return $"{detailedErrorMessage}{innerExceptionMessage}";
+		}
+	}
 }
