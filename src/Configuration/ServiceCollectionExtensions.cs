794eaab0c09f284881cf36afcc6dd6fbb4e170a1
Vladyslav Zaiets <rutova2@gmail.com>
2020-04-14 12:04:43 +0300

    a couple of ServiceCollection exetnsions

--- src/Configuration/ServiceCollectionExtensions.cs
+++ src/Configuration/ServiceCollectionExtensions.cs
@@ -18,10 +18,16 @@ public static class ServiceCollectionExtensions
     /// <summary>
     /// Adds all FFmpeg wrapper services to the DI container.
     /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="configureOptions">Optional action to configure FFmpeg wrapper options.</param>
+    /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is <see langword="null"/>.</exception>
     public static IServiceCollection AddFFmpegWrapper(
         this IServiceCollection services,
         Action<FFmpegWrapperOptions>? configureOptions = null)
     {
+        ArgumentNullException.ThrowIfNull(services);
+
         var options = new FFmpegWrapperOptions();
         configureOptions?.Invoke(options);
 
@@ -29,13 +35,16 @@ public static class ServiceCollectionExtensions
         services.AddSingleton<IMediaRepository, MediaRepository>();
         services.AddSingleton<IOperationRepository, OperationRepository>();
 
-        // Register services
+        // Register services with their required dependencies
         services.AddSingleton<TranscodeService>();
         services.AddSingleton<SubtitleService>();
         services.AddSingleton<ThumbnailService>();
 
-        // Register options
-        services.AddSingleton(options);
+        // Register options if they were configured
+        if (configureOptions is not null)
+        {
+            services.AddSingleton(options);
+        }
 
         return services;
     }
@@ -43,16 +52,28 @@ public static class ServiceCollectionExtensions
     /// <summary>
     /// Adds FFmpeg wrapper with custom configuration options.
     /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="options">The FFmpeg wrapper configuration options.</param>
+    /// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="services"/> or <paramref name="options"/> is <see langword="null"/>.
+    /// </exception>
     public static IServiceCollection AddFFmpegWrapper(
         this IServiceCollection services,
         FFmpegWrapperOptions options)
     {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(options);
+
         return services.AddFFmpegWrapper(opts =>
         {
             opts.DefaultTimeout = options.DefaultTimeout;
             opts.FFmpegPath = options.FFmpegPath;
             opts.FFprobePath = options.FFprobePath;
             opts.LogLevel = options.LogLevel;
+            opts.EnableOperationCaching = options.EnableOperationCaching;
+            opts.MaxCachedOperations = options.MaxCachedOperations;
+            opts.EnableDetailedLogging = options.EnableDetailedLogging;
         });
     }
 }
@@ -62,11 +83,38 @@ public static class ServiceCollectionExtensions
 /// </summary>
 public class FFmpegWrapperOptions
 {
+    /// <summary>
+    /// Gets or sets the default timeout for FFmpeg operations.
+    /// </summary>
     public TimeSpan DefaultTimeout { get; set; } = TimeSpan.FromSeconds(300);
+
+    /// <summary>
+    /// Gets or sets the path to the FFmpeg executable.
+    /// </summary>
     public string? FFmpegPath { get; set; }
+
+    /// <summary>
+    /// Gets or sets the path to the FFprobe executable.
+    /// </summary>
     public string? FFprobePath { get; set; }
+
+    /// <summary>
+    /// Gets or sets the logging level for FFmpeg operations.
+    /// </summary>
     public LogLevel LogLevel { get; set; } = LogLevel.Information;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether operation caching is enabled.
+    /// </summary>
     public bool EnableOperationCaching { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the maximum number of cached operations.
+    /// </summary>
     public int MaxCachedOperations { get; set; } = 1000;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether detailed logging is enabled.
+    /// </summary>
     public bool EnableDetailedLogging { get; set; } = false;
 }
