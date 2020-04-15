056744170c45ee013c3878c6073206b90ef46da5
Vladyslav Zaiets <rutova2@gmail.com>
2020-04-15 08:05:43 +0300

    OperationStats extensions

--- src/Monitoring/OperationStatsExtensions.cs
+++ src/Monitoring/OperationStatsExtensions.cs
@@ -1,11 +1,11 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =====================================================================
+// ===================================================================
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
 
 namespace FFmpegDotnetWrapper.Monitoring
 {
@@ -22,24 +22,23 @@ namespace FFmpegDotnetWrapper.Monitoring
         /// </summary>
         /// <param name="stats">The operation statistics to analyze</param>
         /// <returns>95th percentile execution time in milliseconds, or 0 if no data</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="stats"/> is null</exception>
         public static double Get95thPercentileTimeMs(this OperationStats stats)
         {
+            ArgumentNullException.ThrowIfNull(stats);
+
             if (stats.TotalAttempts == 0)
                 return 0;
 
-            // Create a list of all execution times (simplified approach)
-            // In a real implementation with individual timings, this would use actual recorded times
-            var executionTimes = new List<double>();
-
-            // Calculate based on average and distribution characteristics
-            // For a normal distribution, 95th percentile is roughly mean + 1.645 * stddev
-            // We'll approximate this based on the available statistics
+            // Calculate 95th percentile using a conservative approximation
+            // For skewed distributions common in media processing, 95th percentile is typically
+            // between average and maximum, weighted more heavily toward maximum
             var averageMs = stats.AverageExecutionTime.TotalMilliseconds;
             var maxMs = stats.MaximumExecutionTime.TotalMilliseconds;
 
-            // Simple heuristic: 95th percentile is typically between average and max
-            // Weighted towards max for skewed distributions
-            var percentile95 = averageMs + (maxMs - averageMs) * 0.75;
+            // Conservative estimate: 95th percentile is closer to max for media operations
+            // Use weighted average: 30% average + 70% maximum to avoid outliers dominating
+            var percentile95 = averageMs * 0.3 + maxMs * 0.7;
 
             return Math.Round(percentile95, 2);
         }
@@ -50,8 +49,10 @@ namespace FFmpegDotnetWrapper.Monitoring
         /// </summary>
         /// <param name="stats">The operation statistics to analyze</param>
         /// <returns>Failure rate percentage</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="stats"/> is null</exception>
         public static double GetFailureRate(this OperationStats stats)
         {
+            ArgumentNullException.ThrowIfNull(stats);
             return 100.0 - stats.SuccessRate;
         }
 
@@ -62,32 +63,35 @@ namespace FFmpegDotnetWrapper.Monitoring
         /// <param name="stats">The operation statistics to format</param>
         /// <param name="includeHeader">Whether to include table header</param>
         /// <returns>Formatted table string</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="stats"/> is null</exception>
         public static string ToFormattedTable(this OperationStats stats, bool includeHeader = true)
         {
+            ArgumentNullException.ThrowIfNull(stats);
+
             var lines = new List<string>();
 
             if (includeHeader)
             {
                 lines.Add("╔════════════════════════════════════════════════════════════╗");
-                lines.Add($"║ Operation: {stats.Type,-35}║");
+                lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Operation: {stats.Type,-35}║"));
                 lines.Add("╠════════════════════════════════════════════════════════════╣");
             }
 
-            lines.Add($"║ Total Attempts:   {stats.TotalAttempts,-25} ║");
-            lines.Add($"║ Successful:        {stats.SuccessfulOperations,-25} ║");
-            lines.Add($"║ Failed:            {stats.FailedOperations,-25} ║");
-            lines.Add($"║ Success Rate:      {stats.SuccessRate:0.00}%{new string(' ', 18 - (stats.SuccessRate.ToString("0.00").Length))}║");
-            lines.Add($"║ Failure Rate:      {stats.GetFailureRate():0.00}%{new string(' ', 18 - (stats.GetFailureRate().ToString("0.00").Length))}║");
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Total Attempts: {stats.TotalAttempts,-25} ║"));
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Successful: {stats.SuccessfulOperations,-25} ║"));
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Failed: {stats.FailedOperations,-25} ║"));
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Success Rate: {stats.SuccessRate:0.00}%{new string(' ', 18 - (stats.SuccessRate.ToString("0.00", CultureInfo.InvariantCulture).Length))}║"));
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Failure Rate: {stats.GetFailureRate():0.00}%{new string(' ', 18 - (stats.GetFailureRate().ToString("0.00", CultureInfo.InvariantCulture).Length))}║"));
             lines.Add("╠════════════════════════════════════════════════════════════╣");
-            lines.Add($"║ Total Bytes:       {stats.TotalBytesProcessed:N0,-25} ║");
-            lines.Add($"║ Average Throughput: {stats.AverageThroughputMBps:0.00} MB/s{new string(' ', 15 - (stats.AverageThroughputMBps.ToString("0.00").Length))}║");
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Total Bytes: {stats.TotalBytesProcessed:N0,-25} ║"));
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Average Throughput: {stats.AverageThroughputMBps:0.00} MB/s{new string(' ', 15 - (stats.AverageThroughputMBps.ToString("0.00", CultureInfo.InvariantCulture).Length))}║"));
             lines.Add("╠════════════════════════════════════════════════════════════╣");
-            lines.Add($"║ Average Time:      {stats.AverageExecutionTime.TotalSeconds:0.00}s{new string(' ', 19 - (stats.AverageExecutionTime.TotalSeconds.ToString("0.00").Length))}║");
-            lines.Add($"║ Minimum Time:      {stats.MinimumExecutionTime.TotalSeconds:0.00}s{new string(' ', 19 - (stats.MinimumExecutionTime.TotalSeconds.ToString("0.00").Length))}║");
-            lines.Add($"║ Maximum Time:      {stats.MaximumExecutionTime.TotalSeconds:0.00}s{new string(' ', 19 - (stats.MaximumExecutionTime.TotalSeconds.ToString("0.00").Length))}║");
-            lines.Add($"║ 95th Percentile:   {stats.Get95thPercentileTimeMs():0.00}ms{new string(' ', 17 - (stats.Get95thPercentileTimeMs().ToString("0.00").Length))}║");
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Average Time: {stats.AverageExecutionTime.TotalSeconds:0.00}s{new string(' ', 19 - (stats.AverageExecutionTime.TotalSeconds.ToString("0.00", CultureInfo.InvariantCulture).Length))}║"));
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Minimum Time: {stats.MinimumExecutionTime.TotalSeconds:0.00}s{new string(' ', 19 - (stats.MinimumExecutionTime.TotalSeconds.ToString("0.00", CultureInfo.InvariantCulture).Length))}║"));
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Maximum Time: {stats.MaximumExecutionTime.TotalSeconds:0.00}s{new string(' ', 19 - (stats.MaximumExecutionTime.TotalSeconds.ToString("0.00", CultureInfo.InvariantCulture).Length))}║"));
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ 95th Percentile: {stats.Get95thPercentileTimeMs():0.00}ms{new string(' ', 17 - (stats.Get95thPercentileTimeMs().ToString("0.00", CultureInfo.InvariantCulture).Length))}║"));
             lines.Add("╠════════════════════════════════════════════════════════════╣");
-            lines.Add($"║ Last Updated:      {stats.LastUpdated:yyyy-MM-dd HH:mm:ss}{new string(' ', 15 - (stats.LastUpdated.ToString("yyyy-MM-dd HH:mm:ss").Length))}║");
+            lines.Add(string.Format(CultureInfo.InvariantCulture, $"║ Last Updated: {stats.LastUpdated:yyyy-MM-dd HH:mm:ss}{new string(' ', 15 - (stats.LastUpdated.ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture).Length))}║"));
 
             if (includeHeader)
                 lines.Add("╚════════════════════════════════════════════════════════════╝");
@@ -102,16 +106,17 @@ namespace FFmpegDotnetWrapper.Monitoring
         /// <param name="currentStats">Current statistics</param>
         /// <param name="previousStats">Previous statistics to compare against</param>
         /// <returns>Comparison report showing changes</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="currentStats"/> or <paramref name="previousStats"/> is null</exception>
         public static string CompareWith(this OperationStats currentStats, OperationStats previousStats)
         {
-            if (previousStats == null)
-                return "No previous statistics available for comparison.";
+            ArgumentNullException.ThrowIfNull(currentStats);
+            ArgumentNullException.ThrowIfNull(previousStats);
 
             var report = new System.Text.StringBuilder();
             report.AppendLine("Operation Statistics Comparison Report");
             report.AppendLine("==================================");
             report.AppendLine();
-            report.AppendLine($"Operation Type: {currentStats.Type}");
+            report.AppendLine(string.Format(CultureInfo.InvariantCulture, $"Operation Type: {currentStats.Type}"));
             report.AppendLine();
 
             // Calculate changes
@@ -122,11 +127,11 @@ namespace FFmpegDotnetWrapper.Monitoring
             var timeChange = currentStats.TotalExecutionTime.TotalSeconds - previousStats.TotalExecutionTime.TotalSeconds;
 
             report.AppendLine("Changes:");
-            report.AppendLine($"  Attempts:   {(attemptChange >= 0 ? "+" : "")}{attemptChange}");
-            report.AppendLine($"  Success:     {(successChange >= 0 ? "+" : "")}{successChange}");
-            report.AppendLine($"  Failed:      {(failureChange >= 0 ? "+" : "")}{failureChange}");
-            report.AppendLine($"  Bytes:       {(byteChange >= 0 ? "+" : "")}{byteChange:N0}");
-            report.AppendLine($"  Time:        {(timeChange >= 0 ? "+" : "")}{timeChange:0.0}s");
+            report.AppendLine(string.Format(CultureInfo.InvariantCulture, $" Attempts: {(attemptChange >= 0 ? "+" : "")}{attemptChange}"));
+            report.AppendLine(string.Format(CultureInfo.InvariantCulture, $" Success: {(successChange >= 0 ? "+" : "")}{successChange}"));
+            report.AppendLine(string.Format(CultureInfo.InvariantCulture, $" Failed: {(failureChange >= 0 ? "+" : "")}{failureChange}"));
+            report.AppendLine(string.Format(CultureInfo.InvariantCulture, $" Bytes: {(byteChange >= 0 ? "+" : "")}{byteChange:N0}"));
+            report.AppendLine(string.Format(CultureInfo.InvariantCulture, $" Time: {(timeChange >= 0 ? "+" : "")}{timeChange:0.0}s"));
             report.AppendLine();
 
             // Calculate percentage changes
@@ -134,25 +139,25 @@ namespace FFmpegDotnetWrapper.Monitoring
             var throughputChange = currentStats.AverageThroughputMBps - previousStats.AverageThroughputMBps;
 
             report.AppendLine("Percentage Changes:");
-            report.AppendLine($"  Success Rate:   {successRateChange:+0.00;-0.00;0.00}%");
-            report.AppendLine($"  Throughput:     {throughputChange:+0.00;-0.00;0.00} MB/s");
+            report.AppendLine(string.Format(CultureInfo.InvariantCulture, $" Success Rate: {successRateChange:+0.00;-0.00;0.00}%"));
+            report.AppendLine(string.Format(CultureInfo.InvariantCulture, $" Throughput: {throughputChange:+0.00;-0.00;0.00} MB/s"));
             report.AppendLine();
 
             // Performance indicators
             report.AppendLine("Performance Indicators:");
             if (successRateChange > 5)
-                report.AppendLine("  ✓ Significant success rate improvement detected");
+                report.AppendLine(" ✓ Significant success rate improvement detected");
             else if (successRateChange > 0)
-                report.AppendLine("  ✓ Success rate improvement detected");
+                report.AppendLine(" ✓ Success rate improvement detected");
             else if (successRateChange < -5)
-                report.AppendLine("  ✗ Significant success rate regression detected");
+                report.AppendLine(" ✗ Significant success rate regression detected");
             else if (successRateChange < 0)
-                report.AppendLine("  ✗ Success rate regression detected");
+                report.AppendLine(" ✗ Success rate regression detected");
 
             if (throughputChange > 1.0)
-                report.AppendLine("  ✓ Throughput improvement detected");
+                report.AppendLine(" ✓ Throughput improvement detected");
             else if (throughputChange < -1.0)
-                report.AppendLine("  ✗ Throughput regression detected");
+                report.AppendLine(" ✗ Throughput regression detected");
 
             return report.ToString();
         }
