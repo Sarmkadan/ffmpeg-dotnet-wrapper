5e5209d273ae51fbeb5ab4a01611e8d4b807c143
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-12 10:21:56 +0000

    docs(exceptions): document ProcessExecutionException public API

--- src/Exceptions/ProcessExecutionExceptionExtensions.cs
+++ src/Exceptions/ProcessExecutionExceptionExtensions.cs
@@ -12,7 +12,11 @@ namespace FFmpegDotnetWrapper.Exceptions
 		/// </summary>
 		/// <param name="ex">The exception containing process execution information.</param>
 		/// <returns><see langword="true"/> if the exit code is 0; otherwise, <see langword="false"/>.</returns>
-		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/></exception>
+		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
+		/// <remarks>
+		/// This method checks the <see cref="ProcessExecutionException.ExitCode"/> property to determine success.
+		/// A null exit code will be treated as unsuccessful.
+		/// </remarks>
 		public static bool IsSuccessful(this ProcessExecutionException ex)
 		{
 			ArgumentNullException.ThrowIfNull(ex);
@@ -24,7 +28,10 @@ namespace FFmpegDotnetWrapper.Exceptions
 		/// </summary>
 		/// <param name="ex">The exception containing process execution information.</param>
 		/// <returns>The error output if available; otherwise, the exception message.</returns>
-		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/></exception>
+		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
+		/// <remarks>
+		/// This method prioritizes the <see cref="ProcessExecutionException.ErrorOutput"/> property as the primary error source.
+		/// </remarks>
 		public static string GetErrorMessage(this ProcessExecutionException ex)
 		{
 			ArgumentNullException.ThrowIfNull(ex);
@@ -36,7 +43,10 @@ namespace FFmpegDotnetWrapper.Exceptions
 		/// </summary>
 		/// <param name="ex">The exception containing process execution information.</param>
 		/// <returns>A formatted string containing the error message and exit code information.</returns>
-		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/></exception>
+		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
+		/// <remarks>
+		/// The exit code is formatted as "Exit code: {value}" if available, or "No exit code available" otherwise.
+		/// </remarks>
 		public static string GetDetailedErrorMessage(this ProcessExecutionException ex)
 		{
 			ArgumentNullException.ThrowIfNull(ex);
@@ -50,7 +60,11 @@ namespace FFmpegDotnetWrapper.Exceptions
 		/// </summary>
 		/// <param name="ex">The exception containing process execution information.</param>
 		/// <returns>A formatted string containing the detailed error message and inner exception details.</returns>
-		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/></exception>
+		/// <exception cref="ArgumentNullException"><paramref name="ex"/> is <see langword="null"/>.</exception>
+		/// <remarks>
+		/// This method combines the output of <see cref="GetDetailedErrorMessage(ProcessExecutionException)"/> with inner exception information.
+		/// If no inner exception exists, "No inner exception" is appended to the result.
+		/// </remarks>
 		public static string GetFullExceptionDetails(this ProcessExecutionException ex)
 		{
 			ArgumentNullException.ThrowIfNull(ex);
