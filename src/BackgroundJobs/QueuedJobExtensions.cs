2c9721fb687873da9b1d56b42d9bf5ee48131d57
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 14:50:01 +0300

    Tidy up QueuedJobExtensions

--- src/BackgroundJobs/QueuedJobExtensions.cs
+++ src/BackgroundJobs/QueuedJobExtensions.cs
@@ -13,33 +13,39 @@ namespace FFmpegDotnetWrapper.BackgroundJobs
         /// Gets a string representation of the job's status.
         /// </summary>
         /// <param name="job">The job to get the status for.</param>
-        /// <returns>A string representation of the job's status.</returns>
+        /// <returns>A string representation of the job's status.
+        /// Returns "No due date" if <see cref="QueuedJob.DueAt"/> is null,
+        /// otherwise returns the due date in ISO 8601 format.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="job"/> is null.</exception>
         public static string GetStatusString(this QueuedJob job)
         {
             ArgumentNullException.ThrowIfNull(job);
 
-            return job.DueAt is null ? "No due date" : $"Due at {job.DueAt.Value.ToString("o", CultureInfo.InvariantCulture)}";
+            return job.DueAt is null
+                ? "No due date"
+                : $"Due at {job.DueAt.Value.ToString("o", CultureInfo.InvariantCulture)}";
         }
 
         /// <summary>
         /// Determines whether the job is overdue.
         /// </summary>
         /// <param name="job">The job to check.</param>
-        /// <returns>True if the job is overdue; otherwise, false.</returns>
+        /// <returns>True if the job is overdue; otherwise, false.
+        /// A job is overdue when <see cref="QueuedJob.DueAt"/> is not null and the due date is in the past.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="job"/> is null.</exception>
         public static bool IsOverdue(this QueuedJob job)
         {
             ArgumentNullException.ThrowIfNull(job);
 
-            return job.DueAt is not null && job.DueAt.Value < DateTime.UtcNow;
+            return job.DueAt is { } dueAt && dueAt < DateTime.UtcNow;
         }
 
         /// <summary>
         /// Gets a string representation of the job's retry information.
         /// </summary>
         /// <param name="job">The job to get the retry information for.</param>
-        /// <returns>A string representation of the job's retry information.</returns>
+        /// <returns>A string representation of the job's retry information.
+        /// Format: "Retried {RetryCount} times out of {MaxRetries}".</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="job"/> is null.</exception>
         public static string GetRetryInfoString(this QueuedJob job)
         {
@@ -54,9 +60,15 @@ namespace FFmpegDotnetWrapper.BackgroundJobs
         /// <param name="job">The job to check.</param>
         /// <returns>True if the job has reached its maximum number of retries; otherwise, false.</returns>
         /// <exception cref="ArgumentNullException">Thrown if <paramref name="job"/> is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <see cref="QueuedJob.MaxRetries"/> is negative.</exception>
         public static bool HasMaxRetries(this QueuedJob job)
         {
             ArgumentNullException.ThrowIfNull(job);
+
+            if (job.MaxRetries < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(job.MaxRetries), "MaxRetries cannot be negative.");
+            }
 
             return job.RetryCount >= job.MaxRetries;
         }
