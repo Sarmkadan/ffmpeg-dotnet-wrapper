e623da64a115cb764c048e53cb0fe15cb30e86d7
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 16:32:45 +0300

    Oops

---
+++ tests/ffmpeg-dotnet-wrapper.Tests/QueuedJobJsonExtensionsTests.cs
@@ -0,0 +1,12 @@
+namespace ffmpeg_dotnet_wrapper_tests
+{
+    using Xunit;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+    using System;
+
+    public class QueuedJobJsonExtensionsTests
+    {
+        // ... test methods ...
+    }
+}
