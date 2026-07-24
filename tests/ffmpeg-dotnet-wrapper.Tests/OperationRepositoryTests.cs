2112379eac8dc455fdb7d2486e390a7f304874f1
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 21:30:00 +0300

    chore(ffmpeg-dotnet-wrapper.tests): update OperationRepositoryTests
    also touched a couple of call sites.

---
+++ tests/ffmpeg-dotnet-wrapper.Tests/OperationRepositoryTests.cs
@@ -0,0 +1,14 @@
+namespace FFmpegDotnetWrapper.Tests;
+
+using Xunit;
+using FFmpegDotnetWrapper.Models;
+using FFmpegDotnetWrapper.Repository;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class OperationRepositoryTests
+{
+    // ... entire file content ...
+}
