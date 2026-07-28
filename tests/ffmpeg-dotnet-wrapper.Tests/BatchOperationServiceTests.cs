32481cb01e2df106aa5c1eb2f6d30a3e0f09d813
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-28 08:16:22 +0300

    tweak BatchOperationServiceTests

---
+++ tests/ffmpeg-dotnet-wrapper.Tests/BatchOperationServiceTests.cs
@@ -0,0 +1,10 @@
+namespace FFmpegDotnetWrapper.Tests.Services;
+
+using FFmpegDotnetWrapper.Models;
+using FFmpegDotnetWrapper.Services;
+using Xunit;
+
+public class BatchOperationServiceTests
+{
+    // ... entire content of the file ...
+}
