33837ad6d0301846979ff09ec5240ad2527f2045
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-02 22:17:28 +0000

    chore: apply workspace updates

---
+++ tests/ffmpeg-dotnet-wrapper.Tests/BackgroundJobExtensionsTests.cs
@@ -0,0 +1,57 @@
+using System;
+using Xunit;
+using FFmpegDotNetWrapper.Tests;
+
+namespace FFmpegDotNetWrapper.Tests
+{
+    public class BackgroundJobExtensionsTests
+    {
+        [Fact]
+        public void IsActive_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsCompletedSuccessfully_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsFailed_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetTimeInfo_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetMetadataValue_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void UpdateProgress_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsTakingTooLong_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void GetSummary_Happy_Path_Test()
+        {
+            // Test implementation here
+        }
+    }
+}
